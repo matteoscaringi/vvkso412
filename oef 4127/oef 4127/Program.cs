e161a390add914215d8109fecd449641b8314144
Matteo Scaringi <scaringi.matteo@icloud.com>
2022-12-06 11:41:51 +0100

    added 4.12.7

---
+++ oef 4127/oef 4127/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _412
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
