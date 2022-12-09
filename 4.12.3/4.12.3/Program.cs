8a594dda05a352fde44e3b6120e4ae36688c69ab
Matteo Scaringi <scaringi.matteo@icloud.com>
2022-12-09 17:23:05 +0100

    add 4.12.3

---
+++ 4.12.3/4.12.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _4._12._3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int aantal = 0;
+
+            Console.Write("Hoeveel getallen wil je ingeven: ");
+            aantal = Convert.ToInt32(Console.ReadLine());
+
+            int[] getallen = new int[aantal];
+
+            for (int i = 0; i < aantal; i++)
+            {
+                Console.Write("Geef getal {0}: ", i + 1);
+                getallen[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Array.Reverse(getallen);
+
+            foreach (int i in getallen)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
